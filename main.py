f7dc780a11ff97a33493c83ff53da556399499ef
nootey <poiskusni@gmail.com>
2025-01-22 13:35:28 +0100

    input updates
    - added option to extract mp3 from mp4, if provided
    - added some dependency checks
    - general error handling

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import json
 import os
 import sys
 
 import whisper
 from src import transcribe as t
+from src import summarize as s
 
 
 def print_welcome_message():
@@ -17,12 +19,31 @@ def print_welcome_message():
     print("#" * (width + 2))
 
 def main():
+def test_cuda():
+    """Check if CUDA is available for PyTorch."""
+    cuda_available = torch.cuda.is_available()
+    print("CUDA Available:", cuda_available)
+
+    if cuda_available:
+        print("CUDA Device Name:", torch.cuda.get_device_name(0))
+        print("CUDA Version:", torch.version.cuda)
+    else:
+        print("CUDA not found. Make sure NVIDIA drivers and CUDA Toolkit are installed.")
 
     print_welcome_message()
 
     audio_file = "meeting_example_short"
     audio_file_extension = "mp3"
     audio_file_path = f"data/{audio_file}.{audio_file_extension}"
+def run_dependency_tests():
+    """Run all tests for Ollama, CUDA, and Whisper."""
+    # test_ollama()
+    test_cuda()
+    # test_whisper()
+
+
+def transcribe_audio(file):
+    audio_file_path = t.prepare_audio_file(file)
 
     # Check if file exists
     if os.path.exists(audio_file_path):
@@ -32,6 +53,7 @@ def main():
         sys.exit()
 
     model_name = "small"
+    model_name = "large"
     print(f"Loading model: {model_name}")
     model = whisper.load_model(model_name)
 
@@ -40,8 +62,35 @@ def main():
 
     print("Saving transcription ...")
     t.save_transcription(result["text"], audio_file)
+    t.save_transcription(result["text"], file)
 
     print("Audio transcription complete.")
+def main():
+
+    print("\n")
+    print_welcome_message()
+    print("\n")
+
+    print("Checking for required dependencies...")
+    run_dependency_tests()
+    print("All dependencies are present, proceeding ...")
+    print("\n")
+
+    audio_file = "ims_meeting"
+    json_file_path = f"results/transcriptions/{audio_file}.json"
+
+    # Check if transcription already exists
+    if not os.path.exists(json_file_path):
+        transcribe_audio(audio_file)
+
+    with open(json_file_path, "r", encoding="utf-8") as jf:
+        transcription_data = json.load(jf)
+
+    transcription = transcription_data.get("transcription", "")
+
+    print(f"Transcription [{audio_file}] selected")
+
+    s.create_transcription_summary(transcription, audio_file)
 
 if __name__ == "__main__":
     main()
--- src/transcribe.py
+++ src/transcribe.py
@@ -1,5 +1,37 @@
 import json
 import os
+import sys
+
+from pydub import AudioSegment
+
+def prepare_audio_file(audio_file):
+    directory = "data"
+    mp3_dir = os.path.join(directory, audio_file+".mp3")
+
+    # Find the file with any extension
+    for file in os.listdir(directory):
+        if file.startswith(audio_file):
+            audio_file_extension = os.path.splitext(file)[1][1:]  # Extract extension without dot
+            audio_file_path = os.path.join(directory, file)
+            break
+    else:
+        print("File not found.")
+        sys.exit()
+
+    if audio_file_extension == "mp4":
+        audio_file = extract_mp3(audio_file_path, mp3_dir)
+
+    return audio_file
+
+def extract_mp3(mp4_file, mp3_file):
+    if not os.path.exists(mp4_file):
+        print(f"Error: {mp4_file} does not exist.")
+        return
+
+    print(f"Extracting audio from {mp4_file} to {mp3_file}...")
+    audio = AudioSegment.from_file(mp4_file, format="mp4")
+    audio.export(mp3_file, format="mp3")
+    return mp3_file
 
 def split_into_paragraphs(text, char_limit=300):
     """Splits text into paragraphs based on a character limit,
@@ -23,12 +55,12 @@ def save_transcription(text, filename):
 
     If a file with the same name exists, it appends an index (e.g., file_1.json).
     """
-    json_file_path = f"results/{filename}.json"
+    json_file_path = f"results/transcriptions/{filename}.json"
 
     # Ensure unique filename if file already exists
     index = 1
     while os.path.exists(json_file_path):
-        json_file_path = f"results/{filename}_{index}.json"
+        json_file_path = f"results/transcriptions/{filename}_{index}.json"
         index += 1
 
     # Structure JSON data
